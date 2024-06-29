4cda7b5ee592798d406dc1dbe1c98f5d12d80b29
phovious-14 <krishnpanchal14@gmail.com>
2024-06-29 18:21:41 +0530

    update noun folder

--- src/app/nouns/page.tsx
+++ src/app/nouns/page.tsx
@@ -2,10 +2,6 @@
 
 import Image from "next/image";
 import Navbar from "../../components/navbar/page";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import store from '../../store';
-import Link from "next/link";
-import SearchIcon from '@mui/icons-material/Search';
 
 export default function Home() {
 
@@ -28,44 +24,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="p-[2px] w-[99%] h-full bg-black inria-serif-regular">
-        <div className="bg-white rounded-xl p-12 px-24 w-full">
-          <div className="flex justify-between items-center">
-            <p className="text-black text-[50px] ">Premium Quality Selection</p>
-            <div>
-            <input type="text" className="border-2 p-2 px-4 border-black text-black text-2xl" />
-            <SearchIcon className="text-black absolute right-28 mt-3" />
-            </div>
-          </div>
-          <div className="mt-12 w-full flex justify-start items-center flex-wrap">
-
-            {store.map(item => (
-              <Link className="m-10 card-hover" key={item.id} href={`/product/${item.id}`}>
-                <div className="border-2 sub-border-1 absolute border-black mt-3 ml-3 z-10 rounded-lg bg-[#C2FFB3]">
-                  <Image src={item.image} alt="" className="w-[15rem] h-auto opacity-0" width={"240"} height={"240"} />
-                </div>
-                <div className="border-[3px] rounded-lg relative border-black sub-border-1 bg-white z-20 flex justify-start items-center flex-col w-full">
-                  <div className="text-black text-right mr-2"><FavoriteBorderIcon /></div>
-                  <Image src={item.image} alt="" className="w-[15rem] h-auto" width={"240"} height={"240"} />
-                  <p className="text-black p-2 text-center w-[240px] font-bold text-base border-t-2 border-black">{item.name}</p>
-                  <div className="text-black p-2 text-lg w-full border-t-2 border-black flex justify-around items-center">
-                    <h1>₹ {item.inr}</h1>
-                    |
-                    <div className="flex justify-between items-center flex-row">
-                      <Image src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/base/info/logo.png" alt="" width={"20"} height={"20"} />
-                      <h1 className="ml-2">{item.eth} ETH</h1>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            ))}
-
-
-          </div>
-        </div>
-      </div>
+      </div>    
 
     </div>
   );
